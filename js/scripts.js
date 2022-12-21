ab2de9dcdbda3699ac3d7992efea713d771917c2
Chiaravalle04 <nicolas.chiaravalle@gmail.com>
2022-12-21 18:08:30 +0100

    Esercizio Completato

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,49 @@
+let container = document.createElement('div');
+
+container.classList.add('container');
+
+document.body.appendChild(container);
+
 for (let i = 1; i <= 100; i++) {
+
+    let square = document.createElement('div');
+
+    square.classList.add('square');
+
+    container.appendChild(square);
 
     if (i % 3 === 0 && i % 5 === 0) {
 
       console.log('FizzBuzz');
 
-      document.writeln('FizzBuzz');
+      square.append('FizzBuzz');
+
+      square.classList.add('yellow');
 
     } else if (i % 3 === 0) {
 
       console.log('Fizz');
 
-      document.writeln('Fizz');
+      square.append('Fizz');
+
+      square.classList.add('brown');
 
     } else if (i % 5 === 0) {
 
       console.log('Buzz');
 
+      square.append('Buzz');
+
+      square.classList.add('sandy');
+
     } else {
 
       console.log(i);
 
-      document.writeln(i);
+      square.append(i);
+
+      square.classList.add('turq');
 
     }
-  }
-  
+
+} 
